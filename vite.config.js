b5b4cb0d859c33524e5b972a27a209c80f4eb674
jozimmerman <joshua.zimmerman@lexialearning.com>
2024-08-09 16:01:12 -0500

    updated: resources to meet vite requirements

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "./public/index.html", // Specify the entry point relative to the project root
-    },
-  },
+
   server: {
     open: true, // automatically open the app in the browser
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      screens: path.resolve(__dirname, "./src/screens"),
-    },
+    //port: 3000,
   },
 });
